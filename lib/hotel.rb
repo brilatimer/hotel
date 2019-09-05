d0f706c131e54d08bf50749199f927cae86608ee
brilatimer <brihiramine@gmail.com>
2019-09-04 22:11:14 -0700

    completed track_reservation test

--- lib/hotel.rb
+++ lib/hotel.rb
@@ -20,14 +20,13 @@ class Hotel
     # If no rooms found in the loop, return 
     
     # Loop through rooms until one is available
-    new_reservation = Reservation.new(date_range, room_number)
-    @reservation_list << new_reservation # update list_reservations
+    new_reservation = Reservation.new(date_range: date_range, room_number: nil, confirmation_id: nil, total_cost: nil)
+    @reservation_list << new_reservation 
   end
   
   # based on date range and room number, checks if room is available and returns true
   def is_room_available(date_range:, room_number:)
-    # binding.pry
-    room_match = @reservation_list.select {| reservation| reservation.room_number == room_number && reservation.date_range.check_out  > date_range.check_in && reservation.date_range.check_in <= date_range.check_out} 
+    room_match = @reservation_list.select {| reservation|reservation.room_number == room_number && reservation.date_range.check_out  > date_range.check_in && reservation.date_range.check_in <= date_range.check_out} 
     if room_match.length > 0
       return false
     else 
@@ -40,9 +39,7 @@ class Hotel
     
     # I can access the list of reservations for a specific date, so that I can track reservations by date
     # Use .select to find reservations for a specific date
-    return # list of reservations for given date
+    return []# list of reservations for given date
   end
-  
-  
   
 end
--- test/hotel_test.rb
+++ test/hotel_test.rb
@@ -33,16 +33,34 @@ describe "Hotel class" do
     end
   end
   
+  describe "make_reservation method" do 
+    let(:date_range) { DateRange.new(check_in: Date.new(2019, 9, 10), check_out: Date.new(2019, 9, 15)) }
+    let(:hotel) { Hotel.new(reservation_list:[]) }
+    
+    # check if reservation exists
+    it "can a reservation be made for a given date range" do
+      hotel.make_reservation(date_range: date_range)
+      expect(hotel.reservation_list.first).must_be_kind_of Reservation
+    end
+  end
+  
+  describe "track_reservation for given date method" do
+    let(:reservation1) { Reservation.new(date_range: DateRange.new(check_in: Date.new(2019, 9, 8), check_out: Date.new(2019, 9, 11)), room_number: nil, confirmation_id: nil, total_cost: nil) }
+    
+    let(:reservation2) { Reservation.new(date_range: DateRange.new(check_in: Date.new(2019, 9, 5), check_out: Date.new(2019, 9, 10)), room_number: nil , confirmation_id: nil, total_cost: nil) }
+    
+    let(:reservation3) { Reservation.new(date_range: DateRange.new(check_in: Date.new(2019, 9, 12), check_out: Date.new(2019, 9, 16)), room_number: nil, confirmation_id: nil, total_cost: nil) }
+    
+    let(:hotel) { Hotel.new(reservation_list: [reservation1, reservation2, reservation3])}
+    
+    it "can track reservations by given date through a list of reservations" do 
+      expect(hotel.track_reservation(date: Date.new(2019, 9,10))).must_be_kind_of Array
+      expect(hotel.track_reservation(date: Date.new(2019, 9,10)).length).must_equal 2
+    end
+  end
+  # pass in date time object to track reservations
+  
+  
+  # "test for is_room_available"
 end
 
-# "test for make_reservation"
-# "test for is_room_available"
-
-
-
-
-# "test for track_reservation"
-# if "track_resrvatinon" do 
-#   expect(@hotel.track_reservation(DateTime.new(2019,9,10))) [An array of reservations]
-# end
-# pass in date time object to track reservations
